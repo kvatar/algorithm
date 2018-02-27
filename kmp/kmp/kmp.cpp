6567dd7e24fff7060b325e57b8582897ded1e1bc
kvatar <996026928@qq.com>
2018-02-27 19:52:35 +0800

    完成了kmp next数组运算

--- kmp/kmp/kmp.cpp
+++ kmp/kmp/kmp.cpp
@@ -1,2 +1,41 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+vector<int> GetNext(string str1) {
+	vector<int> vec(str1.length());
+	if (vec.size() == 0) {
+		return vec;
+	}
+	vec[0] = -1;
+
+	if (vec.size() == 1) {
+		return vec;
+	}
+	vec[1] = 0;
+
+	for (int i = 2; i < vec.size(); ++i) {
+		int j = vec[i - 1];
+		while (j != -1 && str1[j] != str1[i - 1]) {
+			j = vec[j];
+		}
+		vec[i] = j + 1;
+	}
+	return vec;
+}
+
+
+
+int main()
+{
+	string str1 = "ababcababck";
+	vector<int> vec = GetNext(str1);
+	for (auto it = vec.begin(); it != vec.end(); ++it) {
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
